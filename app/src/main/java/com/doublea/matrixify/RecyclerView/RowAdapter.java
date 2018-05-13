66edeecaf427eae9f6417f233d24360de9439c59
Ab <absalomCherinet@gmail.com>
2018-05-14 01:37:59 +0200

    Enhancemnet

---
+++ app/src/main/java/com/doublea/matrixify/RecyclerView/RowAdapter.java
@@ -0,0 +1,8 @@
+package com.doublea.matrixify.RecyclerView;
+
+/**
+ * Created by archlinux on 5/13/18.
+ */
+
+public class RowAdapter {
+}
---
+++ app/src/main/java/com/doublea/matrixify/Utils/MyTextView.java
@@ -0,0 +1,8 @@
+package com.doublea.matrixify.Utils;
+
+/**
+ * Created by archlinux on 5/13/18.
+ */
+
+public class MyTextView {
+}
